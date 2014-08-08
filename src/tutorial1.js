05e1c5615276c7f15e3e5887649613bc7df01f84
Jacob Angel <jacob.angel@gmail.com>
2014-08-07 22:58:31 -0400

    I was skeptical but it actually worked.

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -1,12 +1,13 @@
+/** @jsx React.DOM */
 var CommentBox = React.createClass({
   render: function () {
     return (
       <div className="commentBox">
-        Hello, sucker, I'm a frickin' CommentBox
+        Hello, sucker, I am a frickin CommentBox
       </div>
       );
   }
-})
+});
 
 React.renderComponent(
   <CommentBox />,
