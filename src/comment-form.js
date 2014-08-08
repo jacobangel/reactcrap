1c1241adc64f8a795fa4a9ade999376118ad1c46
Jacob Angel <jacob.angel@gmail.com>
2014-08-07 23:36:26 -0400

    Adding click handling stuff.

--- src/comment-form.js
+++ src/comment-form.js
@@ -1,10 +1,30 @@
 /** @jsx React.DOM */
 var CommentForm = React.createClass({
+
+	handleSubmit: function () {
+		var author = this.refs.author.getDOMNode().value.trim();
+		var text = this.refs.text.getDOMNode().value.trim();
+
+		if(!text || !author) {
+			console.log('preventing submission');
+			return false;
+		}
+
+		// @todo presumably wqire this up.
+		this.refs.author.getDOMNode().value = '';
+		this.refs.text.getDOMNode().value = '';
+
+		console.log('Post attempted by: ', author, text);
+		return false;
+	},
+
 	render: function () {
 		return (
-		<div className="commentForm">
-			Hello, world. I am a lowly comment form.
-		</div>
+		<form className="commentForm" onSubmit={this.handleSubmit}>
+			<input type="text" ref="author" placeholder="Name" />
+			<input type="text" ref="text" placeholder="Say something..." />
+			<input type="submit" value="Post" />
+		</form>
 		);
 	}
 });
